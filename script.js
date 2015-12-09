5424f254c2f5978a79739498792511be6c534176
Manny Navarro <manny.navarro@gmail.com>
2015-12-08 18:48:32 -0700

    adding content

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-angular.module('myApp', ['ngMaterial']);
+angular.module('myApp', ['ngMaterial'])
+    .controller('MainCtrl', function($scope){
+        $scope.label='Item One';
+        $scope.description='Awesome Item!';
+    })
+    .controller('SecondaryCtrl', function(){
+        this.label='Item Two';
+        this.description='A better way';
+
+    });
